2cd47ea0e182aa43c8583a9b8d8651e79c786f5c
rachael-t <rnthomas001@gmail.com>
2023-10-12 15:11:31 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `netliby-proxy-redirect-test`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
